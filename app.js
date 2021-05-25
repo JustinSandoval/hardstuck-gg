42b45bcce7ef5f0b02df56ca92e3357bd35ada45
Justin <sandojdev@gmail.com>
2021-05-24 21:06:40 -0700

    added loop to save last 10 games in array

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const methodOverride = require('method-override')
 const path = require('path')
 const getQueueData = require('./src/js/queueRank')
 const getSummonerData = require('./src/js/summoner')
+const getMatchIds = require('./src/js/matchIds')
+const { match } = require('assert')
+const { default: axios } = require('axios')
 require('dotenv').config()
 const API_KEY = process.env.API_KEY
 
@@ -27,9 +30,25 @@ app.get('/lol/search', async (req, res) => {
         const { summoner } = req.query
         const queue = await getQueueData(summoner, API_KEY)
         const profile = await getSummonerData(summoner, API_KEY)
-        console.log(queue)
-        console.log(profile)
-        res.render('search', { queue, profile })
+        const matchids = await getMatchIds(profile.puuid, API_KEY)
+        // console.log(queue)
+        // console.log(profile)
+        // console.log(matchids)
+        let match = []
+        for (let m of matchids) {
+            try {
+                let idk = await axios.get(
+                    `https://americas.api.riotgames.com/lol/match/v5/matches/${m}?api_key=${API_KEY}`
+                )
+                match.push(idk.data)
+            } catch (e) {
+                console.log(`${m} wasnt found`)
+            }
+        }
+
+        console.log(match)
+
+        res.render('search', { queue, profile, match })
     } catch (e) {
         console.dir(e)
     }
